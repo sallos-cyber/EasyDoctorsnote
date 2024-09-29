09790020732117ac2641dc7cadda7cb4cdea5855
Irene <irene@markelic.de>
2024-09-29 15:25:13 +0200

    cleaned code

--- views/dialog_handler.py
+++ views/dialog_handler.py
@@ -1,15 +1,33 @@
 from kivymd.uix.dialog.dialog import MDDialog, MDDialogHeadlineText, MDDialogButtonContainer
 from kivy.uix.widget import Widget
 from kivymd.uix.button import MDButton, MDButtonText
+from typing import Callable, Optional
 
 class DialogHandler:
-    def __init__(self):
+    """
+    A class to handle the display of dialogs in the application.
+
+    Attributes:
+    ----------
+    dialog : Optional[MDDialog]
+        The current dialog instance, if any.
+    """
+    def __init__(self)->None:
         self.dialog = None
 
-    def show_dialog(self, text, close_callback):
-        if not self.dialog:
+    def show_dialog(self, text:str, close_callback:Callable[[], None]) -> None:
+        """
+           Show a dialog with the specified text and a callback for when it is closed.
+
+           Parameters:
+           ----------
+           text : str
+               The text to display in the dialog.
+           close_callback : Callable[[], None]
+               A callback function to be called when the dialog is closed.
+           """
+        if self.dialog is None:
             self.dialog = MDDialog(
-                    
                     MDDialogHeadlineText(
                         text=text,
                         halign="left",
@@ -27,7 +45,7 @@ class DialogHandler:
             self.dialog.text = text
         self.dialog.open()
 
-    def close_dialog(self):
+    def close_dialog(self)->None:
         if self.dialog:
             self.dialog.dismiss()
             self.dialog = None  # Reset dialog reference
