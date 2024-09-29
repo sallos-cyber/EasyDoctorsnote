44e1eceac25570dde26533cc91b6c3f8176e73b1
Irene <irene@markelic.de>
2024-09-29 13:46:12 +0200

    Put MainScreen into its own file

---
+++ views/main_screen.py
@@ -0,0 +1,9 @@
+class MainScreen(MDScreen):
+    """
+    Represents the main screen of the app.
+    Lets you choose if you want to send a "cancel_lunch" and/or a
+    "send_doctors_note" notification to school.
+    """
+    def __init__(self, view_model, **kwargs):
+        super().__init__(**kwargs)
+        self.view_model = view_model
