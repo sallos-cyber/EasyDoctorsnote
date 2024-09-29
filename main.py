c4473c4e1700c1c66882b0d7bce7b3db84d78269
Irene <irene@markelic.de>
2024-09-29 14:10:24 +0200

    cleaned code and added type hints where missing

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.screen import MDScreen
-from kivy.uix.screenmanager import ScreenManager
-from kivy.properties import ObjectProperty, ConfigParserProperty
 from kivy.logger import Logger, LOG_LEVELS
-from views.startup_screen import StartupScreen
-from views.show_email_screen import ShowEmailScreen
-from views.show_done_screen import ShowDoneScreen
-from views.pick_date_screen import PickDateScreen
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+
+from models.data_model import DataModel
 from utils.password_handler import PasswordHandler
 from utils.password_settings import PasswordSettings
 from viewmodels.view_model import ViewModel
-from models.data_model import DataModel
+from views.main_screen import MainScreen
+from views.pick_date_screen import PickDateScreen
+from views.show_done_screen import ShowDoneScreen
+from views.show_email_screen import ShowEmailScreen
+from views.startup_screen import StartupScreen
 
 Logger.setLevel(LOG_LEVELS["debug"])
 
 class ScreenManagement(ScreenManager):
-    
-    def __init__(self, view_model, show_startup:bool, **kwargs):
+    """
+    A class to manage all screens.
+
+    Attributes:
+    ----------
+    view_model : ViewModel
+        The ViewModel contains controls
+    show_startup : bool
+        A config parameter which determines if a startup screen is added or not.
+    """
+    def __init__(self, view_model:ViewModel, show_startup: bool, **kwargs):
         super().__init__(**kwargs)
-        # Gemeinsames Model
-        # model = DataModel()
-        # view_model = ViewModel(model)
         Logger.debug(f'ScreenManagement: {show_startup=}')
         if show_startup:
             startup_screen = StartupScreen(view_model, name='startup_screen')
@@ -35,69 +42,46 @@ class ScreenManagement(ScreenManager):
         self.add_widget(main_screen)
         self.add_widget(pick_date_screen)
         self.add_widget(show_email_screen)
-        self.add_widget(show_done_screen)    
+        self.add_widget(show_done_screen)
 
 
-"""Using MVVM Pattern: First some required user inputs are collected. This data
-is stored in the DataModel class. The check of the data and some control
-is done in the ViewModel class  It also updates the model"""
-
-
-
-
-    
-        
-class MainScreen(MDScreen):
-    
-    """ Lets you choose if you want to send  a "cancel_lunch" and/Or a 
-    "send_doctors_note" notification to school.
-    
-    """  
-
-        
-    def __init__(self, view_model, **kwargs):
-        super().__init__(**kwargs)
-        self.view_model = view_model
-        
-
-        
 class EasyDoctorsnoteApp(MDApp):
-    email_receiver_doctors_note = ConfigParserProperty('defaultvalue','emailconfig','receiveremails_doctorsnote','configparser')
+    """
+    The main entry point into the app EasyDoctorsnote.
 
-    config = ObjectProperty(None) 
-    
-    def build_config(self, config):
-        config.setdefaults( 'emailconfig',{
-            'senderemail':'john@doe.com',
-            'receiveremails_doctorsnote':'xxx@xxx.xx',
+    The app first presents a couple of screens in which user inputs are selected.
+    These are stored in the Config, the ViewModel and Model. Once all the required
+    information is available the according actions can be triggered, which in this
+    case is the sending of emails.
+    """
+    config = ObjectProperty(None)
+
+    def build_config(self, config:dict)-> None:
+        config.setdefaults('emailconfig', {
+            'senderemail': 'john@doe.com',
+            'receiveremails_doctorsnote': 'xxx@xxx.xx',
             'receiveremails_doctorsnote_cc': '',
-            'receiveremails_lunch':'xxx@xxx.xx',
-            'smtpserver':'smtp.xxx.xx',
-            'port':000})
-        config.setdefaults( 'kid',{
-            'kidname':'Jane Doe',
-            'classid':'xx'})
-        config.setdefaults( 'sender',{
-            'sendername':'John Doe',
-            'passwd':'xxx'})
-        config.setdefaults( 'app',{
-            'first_use':'1'})
-        config.setdefaults( 'app',{
+            'receiveremails_lunch': 'xxx@xxx.xx',
+            'smtpserver': 'smtp.xxx.xx',
+            'port': 587})
+        config.setdefaults('kid', {
+            'kidname': 'Jane Doe',
+            'classid': 'xx'})
+        config.setdefaults('sender', {
+            'sendername': 'John Doe',
+            'passwd': 'xxx'})
+        config.setdefaults('app', {
+            'first_use': '1'})
+        config.setdefaults('app', {
             'show_startup': 0})
-        config.setdefaults( 'app',{
+        config.setdefaults('app', {
             'language': 'German'})
 
-
-    
-    def build(self):
-        
+    def build(self)-> ScreenManagement:
         self.pw_handler = PasswordHandler()
-        #self.create_secret_key()  
-
         self.settings_cls = PasswordSettings
         self.use_kivy_settings = False
         self.set_colors()
-
 
         Builder.load_file("kv/main_screen.kv")
         Builder.load_file("kv/pick_date_screen.kv")
@@ -114,42 +98,34 @@ class EasyDoctorsnoteApp(MDApp):
         # Return the screen manager as the root widget
         return self.screen_manager
 
-
-        
-    def on_config_change(self, config, section, key, value):
+    def on_config_change(self, config:dict, section:str, key:str, value:str)-> None:
         if key == 'passwd':
             Logger.debug('EasyDoctorsnoteApp: on_config_change, the user has changed the password!')
-            # Ensure 'value' is in bytes
-            # if isinstance(value, str):
-            #     value = value.encode('utf-8')  # Convert string to bytes
-            
             self.save_passwd(value)
 
-          
-    def save_passwd(self, password):
+    def save_passwd(self, password:str)->None:
         encrypted_password = self.pw_handler.encrypt_password(password)
         self.config.set('sender', 'passwd', encrypted_password.decode())
         self.config.write()
-    def build_settings(self, settings):
-         settings.add_json_panel('Settings Panel', self.config, 'config.json')
-         
-    def if_used_first_time(self):
-        if self.config['app']['first_use']=='1':
 
-            #self.config.write()
+    def build_settings(self, settings)->None:
+        settings.add_json_panel('Settings Panel', self.config, 'config.json')
+
+    def if_used_first_time(self)-> None:
+        if self.config['app']['first_use'] == '1':
             return True
         else:
             return False
-        
-    def show_startup(self):
+
+    def show_startup(self)-> None:
         Logger.debug('NoteApp:, show_startup()')
-        if self.config['app']['show_startup']=='1':
+        if self.config['app']['show_startup'] == '1':
             self.config.write()
             return False
         else:
-            return True        
-             
-    def set_colors(self):
+            return True
+
+    def set_colors(self)-> None:
         self.theme_cls.theme_style_switch_animation = True
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Teal"
@@ -162,12 +138,7 @@ class EasyDoctorsnoteApp(MDApp):
         self.theme_cls.secondary_color = [1.0, 0.75, 0.0, 1]  # Amber
         self.theme_cls.hint_color = [0.5, 0.5, 0.5, 1]  # Grey
         self.theme_cls.error_color = [1.0, 0.0, 0.0, 1]  # Red
-        
 
-    
-if __name__=='__main__':
+if __name__ == '__main__':
     EasyDoctorsnoteApp().run()
-    
 
-class AnotherScreen(MDScreen):
-    pass
